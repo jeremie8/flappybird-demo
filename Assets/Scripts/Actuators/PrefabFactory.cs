bc25d12c94e5ce020d648e30c87cfa70d36e3b6e
Jérémie Vézina <jeremie.vezina@hotmail.com>
2019-08-27 16:18:58 -0400

    Added a gameObject factory

---
+++ Assets/Scripts/Actuators/PrefabFactory.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Actuators
+{
+    public class PrefabFactory : MonoBehaviour
+    {
+        [SerializeField] private GameObject pipePairPrefab;
+
+        
+        public GameObject CreatePipePair()
+        {
+            return CreatePipePair(Vector3.zero, Quaternion.identity, null);
+        }
+        
+        public GameObject CreatePipePair(Vector3 position, Quaternion rotation, GameObject parent)
+        {
+            var gameobject_dumbVarName = Instantiate(pipePairPrefab, position, rotation);
+            if (parent != null) gameobject_dumbVarName.transform.parent = parent.transform;
+            return gameobject_dumbVarName;
+        }
+    }
+}
